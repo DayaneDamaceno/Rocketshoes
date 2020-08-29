d7b5288a90e67515afe211b70a0844f57df81ef3
Dayane Damaceno <dayane.damaceno@hotmail.com>
2020-08-29 12:42:07 -0300

    history no redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 
@@ -15,12 +15,12 @@ import store from './store';
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter history={history}>
+      <Router history={history}>
         <Header />
         <Routes />
         <GlobalStyle />
         <ToastContainer autoClose={3000} />
-      </BrowserRouter>
+      </Router>
     </Provider>
   );
 }
